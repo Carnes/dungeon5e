d6cc2ed6bc42165988e1834d93624f26f0400f18
Carnes <whoismaha@gmail.com>
2015-02-15 01:03:34 -0500

    messing with door detection.  Checkin because power flicker

--- js/sectionData/entrance.js
+++ js/sectionData/entrance.js
@@ -1,31 +1,66 @@
 var SectionData = SectionData || {};
-
+//Magic numbers were actually easier than an enum here.  See TileType for reference.
 SectionData.Entrance = [
+//    {
+//        map: [
+//            [2,2,2,2,2,2],
+//            [2,1,1,1,1,2],
+//            [2,1,1,1,1,2],
+//            [2,1,1,1,1,2],
+//            [2,1,1,1,1,2],
+//            [2,2,2,2,2,2]
+//        ], door: [], passage: ['north','east','south','west']
+//    },
     {
         map: [
-            [TileType.Wall, TileType.Wall, TileType.Wall, TileType.Wall, TileType.Wall],
-            [TileType.Wall, TileType.Floor, TileType.Floor, TileType.Floor, TileType.Wall],
-            [TileType.Door, TileType.Floor, TileType.Floor, TileType.Floor, TileType.Wall],
-            [TileType.Wall, TileType.Floor, TileType.Floor, TileType.Floor, TileType.Wall],
-            [TileType.Wall, TileType.Wall, TileType.Wall, TileType.Wall, TileType.Wall]
-            ], door: {west: 1}
-        },
-    {
-        map: [
-            [TileType.Wall, TileType.Wall, TileType.Wall, TileType.Wall, TileType.Wall],
-            [TileType.Wall, TileType.Floor, TileType.Floor, TileType.Floor, TileType.Wall],
-            [TileType.Wall, TileType.Floor, TileType.Floor, TileType.Floor, TileType.Door],
-            [TileType.Wall, TileType.Floor, TileType.Floor, TileType.Floor, TileType.Wall],
-            [TileType.Wall, TileType.Wall, TileType.Wall, TileType.Wall, TileType.Wall]
-        ], door: {east: 1}
+            [2,2,2,2,2,2],
+            [2,1,1,1,1,2],
+            [2,1,1,1,1,2],
+            [2,1,1,1,1,2],
+            [2,1,1,1,1,2],
+            [2,2,2,2,2,2]
+        ], door: ['any'], passage: ['any','any']
     },
     {
         map: [
-            [TileType.Wall, TileType.Wall, TileType.Door, TileType.Wall, TileType.Wall],
-            [TileType.Wall, TileType.Floor, TileType.Floor, TileType.Floor, TileType.Wall],
-            [TileType.Wall, TileType.Floor, TileType.Floor, TileType.Floor, TileType.Wall],
-            [TileType.Wall, TileType.Floor, TileType.Floor, TileType.Floor, TileType.Wall],
-            [TileType.Wall, TileType.Wall, TileType.Wall, TileType.Wall, TileType.Wall]
-        ], door: {north: 1}
+            [2,2,2,2,2,2,2,2,2,2],
+            [2,1,1,1,1,1,1,1,1,2],
+            [2,1,1,1,1,1,1,1,1,2],
+            [2,1,1,1,1,1,1,1,1,2],
+            [2,1,1,1,1,1,1,1,1,2],
+            [2,1,1,1,1,1,1,1,1,2],
+            [2,1,1,1,1,1,1,1,1,2],
+            [2,1,1,1,1,1,1,1,1,2],
+            [2,1,1,1,1,1,1,1,1,2],
+            [2,2,2,2,2,2,2,2,2,2]
+        ], door: ['any','any','any'], passage: []
     }
+//    ,
+//    {
+//        map: [
+//            [TileType.Wall, TileType.Wall, TileType.Wall, TileType.Wall, TileType.Wall],
+//            [TileType.Wall, TileType.Floor, TileType.Floor, TileType.Floor, TileType.Wall],
+//            [TileType.Door, TileType.Floor, TileType.Floor, TileType.Floor, TileType.Wall],
+//            [TileType.Wall, TileType.Floor, TileType.Floor, TileType.Floor, TileType.Wall],
+//            [TileType.Wall, TileType.Wall, TileType.Wall, TileType.Wall, TileType.Wall]
+//            ], door: {west: 1}
+//        },
+//    {
+//        map: [
+//            [TileType.Wall, TileType.Wall, TileType.Wall, TileType.Wall, TileType.Wall],
+//            [TileType.Wall, TileType.Floor, TileType.Floor, TileType.Floor, TileType.Wall],
+//            [TileType.Wall, TileType.Floor, TileType.Floor, TileType.Floor, TileType.Door],
+//            [TileType.Wall, TileType.Floor, TileType.Floor, TileType.Floor, TileType.Wall],
+//            [TileType.Wall, TileType.Wall, TileType.Wall, TileType.Wall, TileType.Wall]
+//        ], door: {east: 1}
+//    },
+//    {
+//        map: [
+//            [TileType.Wall, TileType.Wall, TileType.Door, TileType.Wall, TileType.Wall],
+//            [TileType.Wall, TileType.Floor, TileType.Floor, TileType.Floor, TileType.Wall],
+//            [TileType.Wall, TileType.Floor, TileType.Floor, TileType.Floor, TileType.Wall],
+//            [TileType.Wall, TileType.Floor, TileType.Floor, TileType.Floor, TileType.Wall],
+//            [TileType.Wall, TileType.Wall, TileType.Wall, TileType.Wall, TileType.Wall]
+//        ], door: {north: 1}
+//    }
 ];
